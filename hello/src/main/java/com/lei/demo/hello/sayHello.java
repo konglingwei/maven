f3539151cc6e7270308e379ed852ea6a9248d028
konglingwei <84227480@qq.com>
2015-08-12 16:38:38 +0800

    f

---
+++ hello/src/main/java/com/lei/demo/hello/sayHello.java
@@ -0,0 +1,12 @@
+package com.lei.demo.hello;
+
+public class sayHello {
+	public void say(){
+		System.out.println("你好啊,我是一个maven打包成jar并运行的demo");
+	}
+	public static void main(String[] args) {
+		new sayHello().say();
+	
+	}
+
+}
