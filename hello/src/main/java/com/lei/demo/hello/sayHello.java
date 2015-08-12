06563648a7ea8cd38b49d8dda00ec4d7c398ea2f
kongling <842227480@qq.com>
2015-08-12 21:36:03 +0800

    1.1.2

--- hello/src/main/java/com/lei/demo/hello/sayHello.java
+++ hello/src/main/java/com/lei/demo/hello/sayHello.java
@@ -2,7 +2,7 @@ package com.lei.demo.hello;
 
 public class sayHello {
 	public void say(){
-		System.out.println("修改以后的sayhello");
+		System.out.println("修改以后的sayhello2");
 	}
 	public static void main(String[] args) {
 		new sayHello().say();
